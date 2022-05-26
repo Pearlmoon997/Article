ac158347cb1a6383aeee0b552cb5992b1fa3084a
moongi <pearlmoon997@gmail.com>
2022-05-26 15:34:15 +0900

    SpringBoot Article

---
+++ src/main/java/com/example/Article/ArticleApplication.java
@@ -0,0 +1,14 @@
+package com.example.Article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleApplication {
+
+	//메인 메소드
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleApplication.class, args);
+	}
+
+}
